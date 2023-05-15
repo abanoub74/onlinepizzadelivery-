4f8cf10441894da33e8645a3c68ef0e0e4f31b92
abanoub <markfady27@gmail.com>
2023-05-15 19:10:53 +0300

    Initial commit

---
+++ partials/_footer.php
@@ -0,0 +1,3 @@
+<div class="footer container-fluid bg-dark text-light">
+    <p class="text-center py-2 mb-0">Designed by <a href="https://github.com/Abdalla2" target="_blank" rel="noopener noreferrer">@Abdallah abd razik</a></p>
+</div>
